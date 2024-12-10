9ee5f573d3baf23532f2edf9ab52053a7aad995d
heitormp <heitormp@protonmail.com>
2024-12-10 14:32:25 +0000

    feature: Sensors

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <pigpio.h>
 #include <thread>
 
+void sensorsThreads(ISensor* sensor) {
+    while(1)
+    {
+        sensor->read();
+        std::cout << "Value from sensor: " << sensor->getType() << ": " << sensor->getValue() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
 int main() {
 
     // CANBus canBus("vcan0", 500000);
@@ -26,7 +35,9 @@ int main() {
 //    }
 
     std::thread drive(&JetSnailsCar::drive, &delorean);
+    std::thread sensor1(sensorsThreads, speedSensor);
     drive.join();
+    sensor1.join();
     
     return 0;
 }
